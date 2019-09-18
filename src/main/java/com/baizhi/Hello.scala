85ee892e9db77f9171ca2fe95127ecb8f89feacd
hyh <2570197124@qq.com>
2019-09-18 16:26:34 +0800

    v12.13

--- src/main/java/com/baizhi/Hello.scala
+++ src/main/java/com/baizhi/Hello.scala
@@ -48,6 +48,7 @@ object Hello {
 
   def h1(ss: String*): Any = {
     print("sss")
+    print("sss")
     /*var a =Array(100,"张三",true,new Date())
     var i= a(new Random().nextInt(4))
     var res=i match {
